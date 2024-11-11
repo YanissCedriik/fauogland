3676df9e224464c8d6fa76dfe1ef018b3ae61731
YanissCedriik <167817702+YanissCedriik@users.noreply.github.com>
2024-11-11 02:38:34 +0100

    Update CSS and added fonts
    Co-authored-by: Jack Rigan <JackXN@users.noreply.github.com>

--- JS/script.js
+++ JS/script.js
@@ -5,8 +5,10 @@ function toggleMenu(){
     const menu = document.getElementById('menu-overlay');
     const burger = document.querySelector('.burger');
     const logo = document.getElementById('logo');
-    
+    const logoToggle = document.getElementById('mobile-logo');
+
     menu.classList.toggle('active');
     burger.classList.toggle('active');
     logo.classList.toggle('logo-color');
+    logoToggle.classList.toggle('logo-toggle');
 }
